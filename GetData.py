289c7c72d11f3a0f54afc9fd774c09dec0c75778
arnemann <jamesarnemann@gmail.com>
2014-07-21 22:00:25 -0700

    Updated getData such that it now returns a 3 member list. The first item (an int) is 1 if ictal, 0 if not. The second number, also an int, is 1 if within the first 15 seconds of the seizure. The final member of the list is the data from the .mat as a pandas dataframe, as before.

--- GetData.py
+++ GetData.py
@@ -1,7 +1,8 @@
 # To be run from the parent data directory, one level above the clip files.
 # Fetches the data from the specified range of clips and returns as
-# a normal list, where each element is a second of the clip data in a
-# pandas array (col_ids = electrode #, row_ids = sample time).
+# a normal list, where each element is a three member list containing
+# a 0 or 1 (interictal, ictal), another 0 or 1 (1 if in first 15 seconds),
+# then finally a pandas dataframe containing the data.
 
 def getData(subject, typ, start, end):
 
@@ -29,8 +30,16 @@ def getData(subject, typ, start, end):
 	
 	for x in range(int(start), int(end)+1):
 		query = subject + '/' + subject + '_' + typ + '_segment_' + str(x).zfill(4) + '.mat'
-		temp = io.loadmat(query)['data']
-		temp = pd.DataFrame(temp).T
+		temp = io.loadmat(query)
+		if (typ == 'ictal'):
+			latency = temp['latency'][0]
+			if (int(latency) < 15):
+				temp = [1, 1, pd.DataFrame(temp['data']).T]
+			else:
+				temp = [1, 0, pd.DataFrame(temp['data']).T]
+		else:
+			temp = [0, 0, pd.DataFrame(temp['data']).T]
+		
 		data.append(temp)
 	
 	return data

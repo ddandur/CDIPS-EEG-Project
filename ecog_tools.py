924f96505934d6384424a00203aeebb85ee77aaa
arnemann <jamesarnemann@gmail.com>
2014-07-29 00:13:55 -0700

    added power function to ecog_tools
    It takes a frequency range and the filename as input, I hope that
    doesn’t mess anything up.

--- ecog_tools.py
+++ ecog_tools.py
@@ -40,6 +40,44 @@ def is_early_ictal(filename):
 
 #number value functions that act on the file names
 #input: a string of the .mat filename
+
+# Power Function: As a first pass, each power function calculates the power spectrum 
+# for all electrodes, averages the spectrum across electrodes, 
+# and finally bins the data into the selected frequency range.. This way the output 
+# from each power function call is still a single real value. 
+ 
+# The lowest sampling frequency is 400 Hz, which means the highest frequency 
+# we should ask about should be 200 Hz. Thus lower_hz should be zero or above 
+# and upper_hz should be 200 and below, with lower_hz < upperhz. 
+# The input variables lower_hz and upper_hz should be integers. 
+
+def power(filename, lower_hz, upper_hz):
+    sample = scipy.io.loadmat(filename)
+#   num_el = number of electrodes     
+    num_el = len(sample['data'])
+    
+# Calculate the real fast fourier transform on each electrode; 
+# the way it's done here is to take modulus of each positive 
+# complex frequency. The result pwr_array is still an array, a list with 
+# num_el values where each element is the corresponding electrode's 
+# values for the FFT squared, each value corresponding to power 
+# at frequency 0 Hz, 1 Hz, etc up to the Nyquist frequency 
+    freq_array = abs(np.fft.rfft(sample['data']))
+    pwr_array = freq_array**2
+    
+# Now average the power across all the electrodes; 
+# ave_pwr is a single array of average power values at each frequency 
+# among the electrodes 
+    sum_pwr = np.sum(pwr_array, axis = 0)
+    ave_pwr = np.sum/float(num_el)
+    
+# Now pick out the power values from the desired frequency range 
+# and sum them. A nice feature for us is that the list index of the elements 
+# in the ave_pwr list is also the frequency corresponding to that element in the list. 
+    selected_part_of_list = ave_pwr[int(lower_hz):int(upper_hz + 1)]
+    return np.sum(selected_part_of_list)
+        
+
 def mean_var(filename):
     sample = scipy.io.loadmat(filename)
     ecog_rec = pd.DataFrame(sample['data'])

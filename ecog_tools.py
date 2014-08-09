65dc24c030e2db3c99c569adbcfbd0f6644338b7
arnemann <jamesarnemann@gmail.com>
2014-08-09 01:05:09 -0700

    streamlined

--- ecog_tools.py
+++ ecog_tools.py
@@ -21,6 +21,8 @@ def labeled_data(functions,data):
 
 #boolean functions that act on the file names
 #input: a string of the .mat filename
+def is_mat(filename):
+    return filename.find('.mat') != -1
 def is_dog(filename):
     return filename.find('Dog') != -1
 def is_human(filename):
@@ -34,64 +36,182 @@ def is_ictal(filename):
 def is_early_ictal(filename):
     if is_ictal(filename):
         tempfile = scipy.io.loadmat(filename)
-        return tempfile['latency'][0] < 15
+        ######## This is under dispute whether < 15 or < 16
+        return tempfile['latency'][0] < 16
     else:
         raise TypeError('file must be ictal')
 
 #number value functions that act on the file names
 #input: a string of the .mat filename
-
-# Power Function: As a first pass, each power function calculates the power spectrum 
-# for all electrodes, averages the spectrum across electrodes, 
-# and finally bins the data into the selected frequency range.. This way the output 
-# from each power function call is still a single real value. 
- 
-# The lowest sampling frequency is 400 Hz, which means the highest frequency 
-# we should ask about should be 200 Hz. Thus lower_hz should be zero or above 
-# and upper_hz should be 200 and below, with lower_hz < upperhz. 
-# The input variables lower_hz and upper_hz should be integers. 
-
-def power(filename, lower_hz, upper_hz):
-    sample = scipy.io.loadmat(filename)
-#   num_el = number of electrodes     
-    num_el = len(sample['data'])
-    
-# Calculate the real fast fourier transform on each electrode; 
-# the way it's done here is to take modulus of each positive 
-# complex frequency. The result pwr_array is still an array, a list with 
-# num_el values where each element is the corresponding electrode's 
-# values for the FFT squared, each value corresponding to power 
-# at frequency 0 Hz, 1 Hz, etc up to the Nyquist frequency 
-    freq_array = abs(np.fft.rfft(sample['data']))
-    pwr_array = freq_array**2
-    
-# Now average the power across all the electrodes; 
-# ave_pwr is a single array of average power values at each frequency 
-# among the electrodes 
-    sum_pwr = np.sum(pwr_array, axis = 0)
-    ave_pwr = sum_pwr/float(num_el)
-    
-# Now pick out the power values from the desired frequency range 
-# and sum them. A nice feature for us is that the list index of the elements 
-# in the ave_pwr list is also the frequency corresponding to that element in the list. 
-    selected_part_of_list = ave_pwr[int(lower_hz):int(upper_hz + 1)]
-    return np.sum(selected_part_of_list)
-        
-
 def mean_var(filename):
     sample = scipy.io.loadmat(filename)
     ecog_rec = pd.DataFrame(sample['data'])
-    return np.mean(ecog_rec.T.var())
+    return [[mean_var.__name__,np.mean(ecog_rec.T.var())]]
 def mean_corr(filename):
     sample = scipy.io.loadmat(filename)
     n_e = len(sample['data'])
     corr_matrix = np.corrcoef(sample['data'])
     np.fill_diagonal(corr_matrix,0)
-    return sum(sum(corr_matrix))/(n_e**2-n_e)
+    return [[mean_corr.__name__,sum(sum(corr_matrix))/(n_e**2-n_e)]]
 def ictal(filename):
-    return int(is_ictal(filename))
+    if is_test(filename):
+        return [[ictal.__name__,np.nan]]
+    return [[ictal.__name__,int(is_ictal(filename))]]
 def early_ictal(filename):
-    if is_ictal(filename):
-        return int(is_early_ictal(filename))
+    if is_test(filename):
+        return [[early_ictal.__name__,np.nan]]
+    elif is_ictal(filename):
+        return [[early_ictal.__name__,int(is_early_ictal(filename))]]
     else:
-        return 0
+        return [[early_ictal.__name__,0]]
+
+#array value functions that act on the file names
+#input: a string of the .mat filename
+def var(filename):
+    sample = scipy.io.loadmat(filename)
+    ecog_rec = pd.DataFrame(sample['data'])
+    var_values = list(ecog_rec.T.var())
+    output_list = []
+    for i in range(len(sample['data'])):
+        if var_values[i] == 0:
+            var_values[i] = np.nan
+        output_list.append(['var_%i'%(i),var_values[i]])
+    return output_list
+
+def corr(filename):
+    sample = scipy.io.loadmat(filename)
+    ecog_rec = pd.DataFrame(sample['data'])
+    var_values = list(ecog_rec.T.var())
+
+    corr_matrix = np.corrcoef(sample['data'])
+    output_list = []
+    for i in range(len(sample['data'])):
+        for j in range(i+1,len(sample['data'])):
+            if var_values[i]==0 or var_values[j]==0:
+                corr_matrix[i,j] = np.nan
+            output_list.append(['corr_%i_%i'%(i,j),corr_matrix[i,j]])
+    return output_list
+
+
+
+
+def power(filename, lower_hz, upper_hz):
+    sample = scipy.io.loadmat(filename)
+    #   num_el = number of electrodes
+    num_el = len(sample['data'])
+    
+    # Calculate the real fast fourier transform on each electrode;
+    # the way it's done here is to take modulus of each positive
+    # complex frequency. The result pwr_array is still an array, a list with
+    # num_el values where each element is the corresponding electrode's
+    # values for the FFT squared, each value corresponding to power
+    # at frequency 0 Hz, 1 Hz, etc up to the Nyquist frequency
+    freq_array = abs(np.fft.rfft(sample['data']))
+    pwr_array = freq_array**2
+    
+    # Now average the power across all the electrodes;
+    # ave_pwr is a single array of average power values at each frequency
+    # among the electrodes
+    sum_pwr = np.sum(pwr_array, axis = 0)
+    ave_pwr = sum_pwr/float(num_el)
+    
+    # Now pick out the power values from the desired frequency range
+    # and sum them. A nice feature for us is that the list index of the elements
+    # in the ave_pwr list is also the frequency corresponding to that element in the list.
+    selected_part_of_list = ave_pwr[int(lower_hz):int(upper_hz + 1)]
+    return np.sum(selected_part_of_list)
+
+
+def deltapower(filename):
+    return [[deltapower.__name__,power(filename,0.1,4)]]
+def thetapower(filename):
+    return [[thetapower.__name__,power(filename,4,8)]]
+def alphapower(filename):
+    return [[alphapower.__name__,power(filename,8,12)]]
+def betapower(filename):
+    return [[betapower.__name__,power(filename,12,30)]]
+def lgammapower(filename):
+    return [[lgammapower.__name__,power(filename,30,70)]]
+def hgammapower(filename):
+    return [[hgammapower.__name__,power(filename,70,180)]]
+
+
+#####
+#####
+#####
+
+
+#intended to transform data into a usable format to train on
+#input: a list of functions and a list of data
+#output: a dictionary with keys being the entries in data
+#note: elements of data must be unique
+def labeled_data(functions,data):
+    dictdata = {}
+    for sample in data:
+        temp_list = []
+        for f in functions:
+            temp_list.append(f(sample))
+        if sample in dictdata:
+            raise TypeError('values in data input not unique')
+        dictdata[sample] = temp_list
+    return dictdata
+
+#creates a dataframe with features generated from one meathod:
+#input: a function that operates on locations that return one value
+#       and a list of datafiles
+#output: corresponding pandas dataframe
+def labeled_dataframe(function,datafiles):
+
+
+    dataframe = pd.DataFrame.from_dict(labeled_data([function],datafiles),orient='index')
+    dataframe.columns = [function.__name__]
+    return dataframe
+
+#overides feature values in csv_file with values in dataframe
+#input: function that need to be added to feature database
+#       and csv_feature_file which is the string of the csv file location
+#       and csv_function_file which is the string of the csv file location
+#output: no return value, modifies the original csv files
+def add_to_featuredb(function, csv_feature_file, csv_function_file):
+    feature_dataframe = pd.DataFrame.from_csv(csv_feature_file)
+    filenames = list(feature_dataframe.index)
+    if len(filenames) != len(set(filenames)):
+        raise TypeError('entries must be uniquely named')
+    new_feature_df = pd.DataFrame()
+    for filename in filenames:
+        if not is_mat(filename):
+            raise TypeError('files must be \'.mat\' files')
+        column_names = []
+        values = []
+        for output in function(filename):
+            column_names.append(output[0])
+            values.append(output[1])
+        value_dataframe = pd.DataFrame([values], index = [filename], columns = column_names)
+        new_feature_df = new_feature_df.append(value_dataframe)
+    column_names = []
+    for output in function(filenames[0]):
+        column_names.append(output[0])
+    function_dataframe = pd.DataFrame.from_csv(csv_function_file)
+    new_function_df = pd.DataFrame([[column_names]],index = [function.__name__], columns = ['column_names'])
+
+    feature_dataframe = new_feature_df.combine_first(feature_dataframe)
+    function_dataframe = new_function_df.combine_first(function_dataframe)
+    feature_dataframe.to_csv(csv_feature_file)
+    function_dataframe.to_csv(csv_function_file)
+
+#creates a dataframe with features only generated from the list of functions
+#input: a list of functions, and the csv_file location as a string
+#output: corresponding pandas dataframe
+def extract_features(functions,csv_feature_file, csv_function_file):
+    function_dataframe = pd.DataFrame.from_csv(csv_function_file)
+    feature_dataframe = pd.DataFrame.from_csv(csv_feature_file)
+    column_names = []
+    for function in functions:
+        if function.__name__ not in function_dataframe.index:
+            raise TypeError('functon ' + function.__name__ + ' has not been evaluated on the feature database')
+        for feature in eval(function_dataframe.get_value(function.__name__,'column_names')):
+            column_names.append(feature)
+    return feature_dataframe.loc[feature_dataframe.index,column_names]
+
+
+
--- training.py
+++ training.py
@@ -1,69 +1,110 @@
-# Training program.
-
 from ecog_tools import *
+import pandas as pd
+import numpy as np
+import sklearn
+import sklearn.ensemble
+import scipy.stats
 from random import shuffle
-from glob import glob
 
-# Create a list of all .mat file names.
-eeg_mat_fnames = glob('*/*.mat')
+total_test_df = pd.DataFrame()
 
-# Seperate files by type.
-test_files = [file for file in eeg_mat_fnames if is_test(file)]
-
-# Shuffle files to seperate for training and validation.
-# This will ulitmately cause a portion to be kept for validation,
-# BUT this portion is different each time, I'm not sure if that
-# is good or bad?
-ictal_files = [file for file in eeg_mat_fnames if is_ictal(file)]
-early_ictal_files = [file for file in ictal_files if is_early_ictal(file)]
-ictal_files = [file for file in ictal_files if not is_early_ictal(file)]
-shuffle(ictal_files)
-shuffle(early_ictal_files)
-inter_ictal_files =[file for file in eeg_mat_fnames if is_inter_ictal(file)]
-shuffle(inter_ictal_files)
-
-
-# Break up into validation and training set.
-# v_percent of the data is withheld for validation.
-v_percent = .25
-
-val_set = early_ictal_files[:int(v_percent*len(early_ictal_files))]
-training_set = early_ictal_files[int(v_percent*len(early_ictal_files)):]
-
-val_set += ictal_files[:int(v_percent*len(ictal_files))]
-training_set += ictal_files[int(v_percent*len(ictal_files)):]
-
-val_set += inter_ictal_files[:int(v_percent*len(inter_ictal_files))]
-training_set += inter_ictal_files[int(v_percent*len(inter_ictal_files)):]
-
-shuffle(val_set)
-shuffle(training_set)
-
-
-# This is where we can add or remove labels and features.
-# All functions act on strings with .mat filenames that can be
-# called in that directory.
-# Suggest adding functions with clear names in 'ecog_tools.py'.
-column_functions = [ictal , early_ictal, mean_corr, mean_var]
-feature_functions = [mean_var,mean_corr]
-
-column_labels = [i.__name__ for i in column_functions]
-feature_labels = [i.__name__ for i in feature_functions]
-
-# Create training data as a dictionary where the keys are file names
-# and the values are a list of values corresponding to the labels
-# and features.
-training_data = labeled_data(column_functions, training_set)
-
-
+subjects = []
+for i in range(1,9):
+    subject = 'Patient_%i'%(i)
+    subjects.append(subject)
+for i in range(1,5):
+    subject = 'Dog_%i'%(i)
+    subjects.append(subject)
 
 ########
-# Use different algorithms to train on training_data
+
+predicted_auc = 0
 
 
+for subject in subjects:
+    
+
+    # Create a list of all .mat file names.
+    ##### This is where you decide which features to train on!
+    #    training_functions  = [early_ictal,ictal,hgammapower]
+
+    #### using this training_functions will run with every single feature in the featuredb csv files!
+    #    uncomment the previous training_functions with the specific functions you want to train on if you don't want to
+    #    include every single function (remember to include ictal and early_ictal) and remove this training_functions
+
+    training_functions = [eval(i) for i in pd.DataFrame.from_csv('%s_functions'%(subject)).index]
+
+    #print training_functions
+
+    feature_dataframe = extract_features(training_functions,'%s_features'%(subject),'%s_functions'%(subject))
+    function_dataframe = pd.DataFrame.from_csv('%s_functions'%(subject))
+
+    eeg_mat_fnames = list(feature_dataframe.index)
+
+    # Seperate files by type.
+    test_files = [file for file in eeg_mat_fnames if is_test(file)]
+
+    # Shuffle files to seperate for training and validation.
+    ictal_files = [file for file in eeg_mat_fnames if is_ictal(file)]
+    early_ictal_files = [file for file in ictal_files if is_early_ictal(file)]
+    ictal_files = [file for file in ictal_files if not is_early_ictal(file)]
+    shuffle(ictal_files)
+    shuffle(early_ictal_files)
+    inter_ictal_files =[file for file in eeg_mat_fnames if is_inter_ictal(file)]
+    shuffle(inter_ictal_files)
+
+    # Break up into validation and training set.
+    # v_percent of the data is withheld for validation.
+    v_percent = 0.20
+
+    val_set = early_ictal_files[:int(v_percent*len(early_ictal_files))]
+    train_set = early_ictal_files[int(v_percent*len(early_ictal_files)):]
+
+    val_set += ictal_files[:int(v_percent*len(ictal_files))]
+    train_set += ictal_files[int(v_percent*len(ictal_files)):]
+
+    val_set += inter_ictal_files[:int(v_percent*len(inter_ictal_files))]
+    train_set += inter_ictal_files[int(v_percent*len(inter_ictal_files)):]
+
+    shuffle(val_set)
+    shuffle(train_set)
 
 
+    training_df = feature_dataframe.loc[train_set]
+    val_df = feature_dataframe.loc[val_set]
+    test_df = feature_dataframe.loc[test_files]
 
+    classifier_labels = ['ictal','early_ictal']
+    feature_labels = list(training_df.drop(classifier_labels, axis=1).columns)
+    column_labels = list(training_df.columns)
 
+    training_df = training_df.dropna()
+    y_test = []
+    y_score = []
+    fpr = [0,0]
+    tpr = [0,0]
+    # calculate ictal and early ictal ROC curves i=0 is ictal, i=1 early ictal
+    for i in range(2):
+        classifier = sklearn.ensemble.RandomForestClassifier(n_estimators=100)
+        classifier.fit(training_df[feature_labels],training_df[classifier_labels])
+        y_test.append(np.array(val_df[classifier_labels[i]]))
+        y_score.append(classifier.predict_proba(val_df[feature_labels])[i].T[1])
+        fpr[i],tpr[i],_ = sklearn.metrics.roc_curve(y_test[i],y_score[i])
 
+    print subject + ' has combined auc of ', (sklearn.metrics.auc(fpr[0], tpr[0]) + sklearn.metrics.auc(fpr[1], tpr[1]))/2.0
+    predicted_auc += len(test_files)*(sklearn.metrics.auc(fpr[0], tpr[0]) + sklearn.metrics.auc(fpr[1], tpr[1]))/2.0
 
+    file_format_dict = {}
+    for test_file in test_files:
+        search_string = '%s_'%(subject)
+        file_format_dict[test_file] = test_file[test_file.find(search_string):]
+    test_df = test_df.apply(lambda x: x.fillna(x.mean()),axis=0)
+    test_df['seizure'] = classifier.predict_proba(test_df[feature_labels])[0].T[1]
+    test_df['early'] = classifier.predict_proba(test_df[feature_labels])[1].T[1]
+    test_df = test_df.drop([i for i in test_df.columns if i != 'seizure' and i != 'early'],axis =1)
+    test_df = test_df.rename(index=file_format_dict)
+    total_test_df = total_test_df.combine_first(test_df)
+total_test_df.index.name = 'clip'
+total_test_df.to_csv('submission.csv')
+print 'len: ', len(total_test_df), ' which should be 32915'
+print 'predicted auc: ', predicted_auc/len(total_test_df)
